1a69be7e2368d73678f19a749a93a36b301f82e9
Kevin-Stephens <kmstephens437@gmail.com>
2023-07-16 17:59:24 -0500

    update html and JS

---
+++ Assets/script.js
@@ -0,0 +1,11 @@
+//declare globals
+
+var timeLeft = 0;
+var score = 0;
+var questionNumber = 1;
+
+//declare question/answer variables
+
+var questions[{question:""answers:""selection:""},{question:""answers:""selection:""}]
+
+c
